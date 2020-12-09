6aeff95e92911497d20f040a3435e8d52120e0db
adifranco2 <adifranco2@gmail.com>
2020-12-08 20:46:15 -0500

    Add project files.

---
+++ Step_451/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_451
+{
+    class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+
+        public ICollection<Student> SStudents { get; set; }
+    }
+}
---
+++ Step_451/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_451
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jane Smith" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
